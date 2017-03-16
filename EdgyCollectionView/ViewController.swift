c1d64be542be633da072c3abfcb48ad65b89af19
Yuichi Fujiki <yuichi@chaione.com>
2017-03-16 16:45:33 -0500

    Initial Commit

---
+++ EdgyCollectionView/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  EdgyCollectionView
+//
+//  Created by Yuichi Fujiki on 3/16/17.
+//  Copyright © 2017 yfujiki. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
